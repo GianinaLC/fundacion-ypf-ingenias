2f4b91eb60e12ea8d244e031e6a0d106ed2633d6
glcarranzani <glcarranzani@gmail.com>
2022-11-30 15:27:24 -0300

    Se crea json con la info que va en las card de castraciones

--- js/castraciones.js
+++ js/castraciones.js
@@ -1,4 +1,26 @@
 ///CASTRACIONES
+const BASE_CASTN = '../data/cast.json'
+
+fetch(BASE_CASTN)
+    .then(response => response.json())
+    .then(json => showCharacters(json));
+
+const showCharacters = (huellitaJson) => {
+    let div = document.getElementById("castracion");
+    for (const huellita of huellitaJson) {
+        let div2 = document.createElement("div");
+        div2.innerHTML = `<article class="fotosC">
+                            <img src="${huellita.imagen}" alt="${huellita.id}, ${huellita.nombre}" class="imagenCast">
+                            <div class="textoCast">
+                                <div class="motivoCast">${huellita.nombre}</div>
+                                <div class="desCast">${huellita.descripcion}.</div>
+                            </div>
+                        </article>`;
+        div.appendChild(div2);
+    }
+}
+
+
 let btnConsultar = document.getElementById('btnConsultar')
 btnConsultar.addEventListener('click', mostrarLugar)
 
